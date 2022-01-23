b0f16dbdb2f9951ae20e0eaae7f8b2a787044570
Neil Mosser <david.mosser7207@gmail.com>
2022-01-23 10:05:42 -0700

    Initial file structure set up

---
+++ backend/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+app.use(express.static(__dirname + '/../../frontend/dist/'));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const App = () => {
+
+  return (
+    <div>
+      Works
+    </div>
+  )
+}
+
+export default App;

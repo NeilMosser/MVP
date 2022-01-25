178acce8b1c479a1c0d21bb8aeb739e5de46ae32
Neil Mosser <david.mosser7207@gmail.com>
2022-01-25 13:08:33 -0600

    Added default video; working state before lyric finder

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -3,9 +3,10 @@ import YouTube from '@u-wave/react-youtube';
 
 const VideoPlayer = () => {
 
+  const videoID = "G97_rOdHcnY";
   return(
     <YouTube className="youtube-player"
-  video="G97_rOdHcnY"
+  video={videoID}
   width="50%"
   height="400px"
 />
